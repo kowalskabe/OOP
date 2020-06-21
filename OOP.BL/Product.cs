af1b614aed650855a2c5113dc33ad015e3925184
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-06-21 19:44:48 +0200

    Adding product repository

--- OOP.BL/Product.cs
+++ OOP.BL/Product.cs
@@ -20,19 +20,10 @@ namespace OOP.BL
         public int ProductId { get; private set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
-        public float? CurrentPrice { get; set; }
+        public double? CurrentPrice { get; set; }
 
 
-        public Product Retrieve(int productId)
-        {
-            return new Product();
-        }
-
-        public bool Save()
-        {
-            return true;
-        }
-
+        
         public bool Validate()
         {
             var isValid = true;
---
+++ OOP.BL/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.BL
+{
+    class ProductRepository
+    {
+        public Product Retrieve(int productId)
+        {
+            Product product = new Product(productId);
+
+            //temp
+            if (productId == 2)
+            {
+                product.ProductName = "sunflower";
+                product.ProductDescription = "Assorted size set of 4 bright yellow mini sunflowers";
+                product.CurrentPrice = 15.96;
+            }
+
+            return product;
+        }
+
+        public bool Save(Product product)
+        {
+            return true;
+        }
+    }
+}
