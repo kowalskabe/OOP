bf53f6bbecf17b4e98aa55f7fbd24d658379f06b
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-06-21 19:56:12 +0200

    Adding order repository class

--- OOP.BL/Order.cs
+++ OOP.BL/Order.cs
@@ -23,16 +23,7 @@ namespace OOP.BL
         public int OrderId { get; private set; }
 
 
-        public Order Retrieve(int orderId)
-        {
-            return new Order();
-        }
-
-        public bool Save()
-        {
-            return true;
-        }
-
+    
         public bool Validate()
         {
             var isValid = true;
---
+++ OOP.BL/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.BL
+{
+    class OrderRepository
+    {
+        public Order Retrieve(int orderId)
+        {
+            Order order = new Order(orderId);
+
+            //tmp
+            if (orderId == 10)
+            {
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 6, 21, 12, 00, 00, new TimeSpan(7, 0, 0));
+            }
+
+            return order;
+        }
+
+        public bool Save(Order order)
+        {
+            return true;
+        }
+    }
+}
