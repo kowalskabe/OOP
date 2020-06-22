562c52a38beb1888526eb38a74f2830ea4f14917
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-06-22 17:33:32 +0200

    Adding OrderItems list to orders.

--- OOP.BL/Order.cs
+++ OOP.BL/Order.cs
@@ -8,7 +8,7 @@ namespace OOP.BL
 {
     public class Order
     {
-        public Order()
+        public Order() : this(0)
         {
 
         }
@@ -16,14 +16,18 @@ namespace OOP.BL
         public Order(int orderId)
         {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
 
+        public int CustomerId { get; set; }
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public int ShippingAddressId { get; set; }
 
 
-    
+
         public bool Validate()
         {
             var isValid = true;
